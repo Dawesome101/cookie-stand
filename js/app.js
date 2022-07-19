8b767d19e05c79221c6d19b11134890678cf405c
Dawesome101 <dawesome@danawesome.com>
2022-07-18 20:33:44 -0700

    Added a complete and functional Object Literal as a template for the rest.  Creating a commit to use as a restore point if anything should go wrong.

--- js/app.js
+++ js/app.js
@@ -1,36 +1,67 @@
 'use strict';
 
-let Seattle = {
+let seattle = {
   storeName: 'Seattle',
   hourlyCust: {
     min: 23,
     max: 65
   },
   avgCookiesPerCust: 6.3,
+  cookiePerHourEntries: 15, //Always add one to this number because the last slot in the array that uses it is the total of all the cookies per hour. So make sure to add one additional slot.
 
-  randomNumOfCustPerHour: function(){
+  listItemData: [],
+  cookiesTotal: 0,
+
+  calcCookiesPerHour: function () {
+    for (let i = 0; i < this.cookiePerHourEntries; i++) {
+
+      let tempNum = Math.round(this.randomNumOfCustPerHour() * this.avgCookiesPerCust);
+      this.cookiesTotal += tempNum;
+
+      if (i < 6) {
+        this.listItemData[i] = `${i + 6}am: ${tempNum} cookies`;
+      } else if (i === 6) {
+        this.listItemData[i] = `${i + 6}pm: ${tempNum} cookies`;
+      } else if (i > 6 && i < this.cookiePerHourEntries - 1) {
+        this.listItemData[i] = `${i - 6}pm: ${tempNum} cookies`;
+      } else {
+        this.listItemData[i] = `Total: ${this.cookiesTotal} cookies`;
+      }
+    }
+  },
+
+  randomNumOfCustPerHour: function () {
     return Math.floor(Math.random() * (this.hourlyCust.max - this.hourlyCust.min) + this.hourlyCust.min);
   },
 
-  calcCookiesPerHour: function(){
-    let tempArray = [0];
-    let tempTextArray = [''];
-    for (let i = 0; i < 14; i++){
+  render: function () {
+    let parentElem = document.getElementById('sales-info');
+    let articleElem = document.createElement('article');
+    parentElem.appendChild(articleElem);
 
-      tempArray[i] = this.randomNumOfCustPerHour();
+    let h2Elem = document.createElement('h2');
+    h2Elem.textContent = this.storeName;
+    articleElem.appendChild(h2Elem);
 
-      if(i < 6 ){
-        tempTextArray[i] = `${i + 6}am: ${Math.round(tempArray[i] * this.avgCookiesPerCust)} cookies`;
-      } else if(i === 6) {
-        tempTextArray[i] = `${i + 6}pm: ${Math.round(tempArray[i] * this.avgCookiesPerCust)} cookies`;
-      } else {
-        tempTextArray[i] = `${i - 6}pm: ${Math.round(tempArray[i] * this.avgCookiesPerCust)} cookies`;
+    let pElem = document.createElement('p');
+    articleElem.appendChild(pElem);
+
+    let ulElem = document.createElement('ul');
+    articleElem.appendChild(ulElem);
+
+    this.calcCookiesPerHour();
+
+    for (let i = 0; i <= this.listItemData.length; i++) {
+      if (i < this.listItemData.length) {
+        let liElem = document.createElement('li');
+        liElem.textContent = this.listItemData[i];
+        ulElem.appendChild(liElem);
       }
     }
-    return tempTextArray;
-  },
 
-  render: function(){
-
+    let imgElem = document.createElement('img');
+    articleElem.appendChild(imgElem);
   }
 };
+
+seattle.render();
