281bb8719c2b8eb1c43e1fc0e03e3ae1ba660fe9
Dawesome101 <dawesome@danawesome.com>
2022-07-19 20:13:42 -0700

    Completed the footer section 'Totals' and cleaned up the code to be as neat and dry as possible

--- js/app.js
+++ js/app.js
@@ -1,91 +1,109 @@
 'use strict';
 
 let stores = [];
+let combinedTotalForEachHour = [];
 let globalOpenTime = 14;
 let tableCells = globalOpenTime + 1;
 
-function Store(storeName, minCust, maxCust, aveCookiesPerCust){
+function Store(storeName, minCust, maxCust, aveCookiesPerCust) {
   this.storeName = storeName;
-  this.minCust = minCust;
-  this.maxCust = maxCust;
-  this.aveCookiesPerCust = aveCookiesPerCust;
+  if (storeName !== 'Totals') {
+    this.minCust = minCust;
+    this.maxCust = maxCust;
+    this.aveCookiesPerCust = aveCookiesPerCust;
+  }
 
   let storeSalesData = [];
-
   this.calculateStoreData(this);
+
   stores.push(this);
 }
 
-Store.prototype.calculateStoreData = function(store){
+Store.prototype.calculateStoreData = function (store) {
   let tempDataArray = [];
-  for(let i = 0; i <= globalOpenTime; i++){
+  for (let i = 0; i <= globalOpenTime; i++) {
     tempDataArray[i] = (Math.round(Math.floor(Math.random() * (store.maxCust - store.minCust) + store.minCust) * store.aveCookiesPerCust));
   }
   store.storeSalesData = tempDataArray;
 };
 
-Store.prototype.render = function(){
-  let tbodyElm = document.getElementById('table-body');
-  let tbodytrElm = document.createElement('tr');
-  tbodyElm.appendChild(tbodytrElm);
-
-  let tfootElm = document.getElementById('table-foot');
-  let tfoottrElm = document.createElement('tr');
-  tfootElm.appendChild(tfoottrElm);
-
+Store.prototype.render = function () {
+  let tbodytrElm;
+  let tfoottrElm;
   let dailyTotal = 0;
 
-  for(let i = 0; i <= tableCells; i++){
-    if(i === 0){
-      let tbodytdElm = document.createElement('td');
-      tbodytrElm.appendChild(tbodytdElm);
-      tbodytdElm.textContent = this.storeName;
-    } else if (i < tableCells){
-      dailyTotal += this.storeSalesData[i - 1];
-      let tbodytdElm = document.createElement('td');
-      tbodytrElm.appendChild(tbodytdElm);
-      tbodytdElm.textContent = this.storeSalesData[i - 1];
+  if (this.storeName !== 'Totals') {
+    let tbodyElm = document.getElementById('table-body');
+    tbodytrElm = document.createElement('tr');
+    tbodyElm.appendChild(tbodytrElm);
+  } else {
+    let tfootElm = document.getElementById('table-foot');
+    tfoottrElm = document.createElement('tr');
+    tfootElm.appendChild(tfoottrElm);
+  }
+
+  for (let i = 0; i <= tableCells; i++) {
+    if (i === 0) {
+      if (this.storeName !== 'Totals') {
+        let tbodytdElm = document.createElement('td');
+        tbodytrElm.appendChild(tbodytdElm);
+        tbodytdElm.textContent = this.storeName;
+      } else {
+        let tfootthElm = document.createElement('th');
+        tfoottrElm.appendChild(tfootthElm);
+        tfootthElm.textContent = this.storeName;
+      }
+    } else if (i < tableCells) {
+      if (this.storeName !== 'Totals') {
+        dailyTotal += this.storeSalesData[i - 1];
+        let tbodytdElm = document.createElement('td');
+        tbodytrElm.appendChild(tbodytdElm);
+        tbodytdElm.textContent = this.storeSalesData[i - 1];
+      } else {
+        dailyTotal += combinedTotalForEachHour[i - 1];
+        let tfootthElm = document.createElement('th');
+        tfoottrElm.appendChild(tfootthElm);
+        tfootthElm.textContent = combinedTotalForEachHour[i - 1];
+      }
     } else {
-      let tbodytdElm = document.createElement('td');
-      tbodytrElm.appendChild(tbodytdElm);
-      tbodytdElm.textContent = dailyTotal;
+      if (this.storeName !== 'Totals') {
+        let tbodytdElm = document.createElement('td');
+        tbodytrElm.appendChild(tbodytdElm);
+        tbodytdElm.textContent = dailyTotal;
+      } else {
+        let tfootthElm = document.createElement('th');
+        tfoottrElm.appendChild(tfootthElm);
+        tfootthElm.textContent = dailyTotal;
+      }
     }
   }
 };
-
-
 
 let seattle = new Store('Seattle', 23, 65, 6.3);
 let tokyo = new Store('Tokyo', 3, 24, 1.2);
 let dubai = new Store('Dubai', 11, 38, 3.7);
 let paris = new Store('Paris', 20, 38, 2.3);
 let Lima = new Store('Lima', 2, 16, 4.6);
+let totals = new Store('Totals', NaN, NaN, NaN);
 
 createTable();
 renderAll();
 
-function renderAll(){
-  let tempStoreTotalForHour = [];
-
-  for(let i = 0; i < stores.length; i++){
+function renderAll() {
+  for (let i = 0; i < stores.length; i++) {
     stores[i].render();
-    for (let j = 0; j < stores[i].storeSalesData.length; j++){
+    for (let j = 0; j < stores[i].storeSalesData.length; j++) {
 
-      if(tempStoreTotalForHour[j] === undefined)
-      {
-        tempStoreTotalForHour[j] = stores[i].storeSalesData[j];
+      if (combinedTotalForEachHour[j] === undefined) {
+        combinedTotalForEachHour[j] = stores[i].storeSalesData[j];
       } else {
-        tempStoreTotalForHour[j] += stores[i].storeSalesData[j];
+        combinedTotalForEachHour[j] += stores[i].storeSalesData[j];
       }
     }
   }
 }
 
-
-
-
-
-function createTable(){
+function createTable() {
   let parentElm = document.getElementById('sales-table');
   let captionElm = document.createElement('caption');
   parentElm.appendChild(captionElm);
@@ -98,8 +116,7 @@ function createTable(){
   let headtrElm = document.createElement('tr');
   theadElm.appendChild(headtrElm);
 
-  for(let i = 0; i <= tableCells; i++)
-  {
+  for (let i = 0; i <= tableCells; i++) {
     let headthElm = document.createElement('th');
     headtrElm.appendChild(headthElm);
 
@@ -107,9 +124,9 @@ function createTable(){
       headthElm.textContent = `${i + 5}am:`;
     } else if (i === 7) {
       headthElm.textContent = `${i + 5}pm:`;
-    } else if (i > 7 && i < tableCells ) {
+    } else if (i > 7 && i < tableCells) {
       headthElm.textContent = `${i - 7}pm:`;
-    } else if (i === tableCells ){
+    } else if (i === tableCells) {
       headthElm.textContent = 'Daily Location Total';
     }
   }
@@ -122,4 +139,3 @@ function createTable(){
   parentElm.appendChild(tfootElm);
   tfootElm.setAttribute('id', 'table-foot');
 }
-
